f83f50f8c96772277553aa378bdd87a2312aa5a1
Rohan Rajesh <your-email@example.com>
2026-05-31 21:38:36 +0530

    Created S3 bucket and uploaded index.html using Terraform

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
