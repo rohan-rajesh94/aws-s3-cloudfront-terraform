1878387adfbe93ecac8381d6fc388d8701fd8b32
Rohan Rajesh <your-email@example.com>
2026-06-01 15:22:56 +0530

    Completed S3 CloudFront Terraform assignment

--- main.tf
+++ main.tf
@@ -19,5 +19,41 @@ resource "aws_s3_object" "index" {
   bucket       = aws_s3_bucket.website_bucket.id
   key          = "index.html"
   source       = "index.html"
+  etag         = filemd5("index.html")
   content_type = "text/html"
+}
+resource "aws_cloudfront_distribution" "website_cdn" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "bucket_name" {
   value = aws_s3_bucket.website_bucket.bucket
 }
+
+output "cloudfront_url" {
+  value = "https://${aws_cloudfront_distribution.website_cdn.domain_name}"
+}
